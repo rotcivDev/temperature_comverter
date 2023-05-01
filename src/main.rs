482aad10111360539b20d1075f25cb78c2e75613
Victor Salles <vsallesa@gmail.com>
2023-04-30 22:38:33 -0300

    refactor: change if by match

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use std::io;
 
 fn main() {
     println!("Temperature Converter - between Celsius and Fahrenheit.");
+    
     loop {
-        println!("Press \n 1- To convert from C to F\n 2- To convert from F to C\n9- To exit");
+        println!("Press \n 1- To convert from C to F\n 2- To convert from F to C\n 9- To exit");
 
         const EXIT_CHOICE: u32 = 9;
-        const INVALID_CHOICE: u32 = 0;
 
         let mut unit = String::new();
         io::stdin()
@@ -15,40 +15,28 @@ fn main() {
 
         let choice = choice_handler(&unit);
 
-        if choice == INVALID_CHOICE {
-            println!("invalid input");
+        match choice {
+            1 => celsius_to_fahrenheit(),
+            2 => fahrenheit_to_celsius(),
+            EXIT_CHOICE => break,
+            _ => println!("invalid input"),
         }
-        if choice == EXIT_CHOICE {
-            break;
-        }
-        if choice == 1 {
-            celsius_to_fahrenheit();
-        }
-        if choice == 2 {
-            fahrenheit_to_celsius();
-        }
+
     }
 }
+
 fn choice_handler(value: &String) -> u32 {
     let value: u32 = match value.trim().parse() {
         Ok(num) => num,
         Err(_) => 0,
     };
 
-    if value == 1 {
-        return value;
+    match value {
+        1 | 2 | 9 => value,
+        _ => 0,
     }
-
-    if value == 2 {
-        return value;
-    }
-
-    if value == 9 {
-        println!("exiting");
-        return value;
-    }
-    0
 }
+
 fn celsius_to_fahrenheit() {
     loop {
         let mut temperature = String::new();
@@ -66,6 +54,7 @@ fn celsius_to_fahrenheit() {
         break;
     }
 }
+
 fn fahrenheit_to_celsius() {
     loop {
         let mut temperature = String::new();
